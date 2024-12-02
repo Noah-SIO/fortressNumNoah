10530834add83064a21a7830a04d874136d6abd0
Noah_FGC <laplaze.noah@gmail.com>
2024-12-02 11:12:27 +0100

    ajout explication utilisation code

--- index.php
+++ index.php
@@ -5,12 +5,15 @@ require_once("class/utilisateur.php");
 
 if(isset($_GET['code'])){
     if($_GET['code'] == "4AGERB26"){
+        require_once("view/expli.php");
         require_once("view/def2.php");
     }
     if($_GET['code'] == "FN2FK2"){
+        require_once("view/expli.php");
         require_once("view/def3.php");
     }
     if($_GET['code'] == "B16BF20C"){
+        require_once("view/expli.php");
         require_once("view/def4.php");
     }
     if($_GET['code'] != "B16BF20C" && $_GET['code'] != "FN2FK2" && $_GET['code'] != "4AGERB26"){
@@ -19,5 +22,6 @@ if(isset($_GET['code'])){
 }
 else{
     require_once("view/accueil.php");
+    require_once("view/expli.php");
     require_once("view/def1.php"); 
 }
---
+++ view/expli.php
@@ -0,0 +1,7 @@
+<?php
+echo"<hr>";
+echo "<h2>Comment accèder aux pages</h2>";
+echo"<p>Une fois le code récuperer veuillez l'insérer dans l'url :</p>";
+echo"<p>comme cet exemple, http://localhost/fortressNumNoah/?code=votreCode </p>";
+echo"<hr></br>";
+?>
