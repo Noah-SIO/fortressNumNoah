3941d912cb86e748337f193951b4ea35607a7a32
Noah_FGC <laplaze.noah@gmail.com>
2024-12-05 13:06:25 +0100

    er to ée

--- view/lose.php
+++ view/lose.php
@@ -1,4 +1,4 @@
 <?php
 echo"<h1>Mauvaise Réponse</h1>";
-echo"<p>L'IA à attaquer le système</p>";
+echo"<p>L'IA à attaquée le système</p>";
 echo"<p style='font-size: 100px '> 💀  💻 </p>";
--- view/win.php
+++ view/win.php
@@ -1,4 +1,4 @@
 <?php
 echo"<h1>Vous avez Réussi !!!</h1>";
-echo"<p>L'IA à été arrêter</p>";
+echo"<p>L'IA à été arrêtée</p>";
 echo"<p style='font-size: 100px '> 🎉  🎉 </p>";
